05eea17b92d0f532478761491e6e1067c3da0b54
Yorick Terweijden <yorick.terweijden@sourcefabric.org>
2013-07-05 11:34:24 +0200

    Implements the IntDateFormatter, now fully localised!

--- newscoop/include/smarty/campsite_plugins/function.daterange_calendar_html.php
+++ newscoop/include/smarty/campsite_plugins/function.daterange_calendar_html.php
@@ -17,31 +17,32 @@
  *      The requested calendar as HTML
  *
  * @example
- *  {{ daterange_calendar_html rangestart="2013-10-30" rangeend="2014-10-30" rangeformatmonth="m" rangeformatday="d"}}
+ *  {{ daterange_calendar_html rangestart="2013-10-30" rangeend="2014-10-30" rangeformatmonth="MM" rangeformatday="dd" locale="de-DE"}}
+ *  For the full range of formatting possible please refer to http://userguide.icu-project.org/formatparse/datetime
  *
  */
 
 function smarty_function_daterange_calendar_html($p_params = array(), &$p_smarty)
 {
     // The $p_params override the $_GET
-    $acceptedParams = array('rangestart', 'rangeend', 'rangeformat', 'rangeformatmonth', 'rangeformatday');
+    $acceptedParams = array('rangestart', 'rangeend', 'rangeformat', 'rangeformatmonth', 'rangeformatday', 'locale');
     $cleanParam = array();
 
     foreach ($acceptedParams as $key) {
-        if (array_key_exists($key, $p_params) && !empty($p_params[$key])) {
-            $cleanParam[$key] = $p_params[$key];
-        } else if (array_key_exists($key, $_GET) && !empty($_GET[$key])) {
+        if (array_key_exists($key, $_GET) && !empty($_GET[$key])) {
             $cleanParam[$key] = $_GET[$key];
+        } else if (array_key_exists($key, $p_params) && !empty($p_params[$key])) {
+            $cleanParam[$key] = $p_params[$key];
         }
     }
 
-    $cleanParam['rangeformatmonth'] = (!array_key_exists('rangeformatmonth', $cleanParam)) ? 'm' : $cleanParam['rangeformatmonth'];
-    $cleanParam['rangeformatday'] = (!array_key_exists('rangeformatday', $cleanParam)) ? 'd' : $cleanParam['rangeformatday'];
+    $cleanParam['rangeformatmonth'] = (!array_key_exists('rangeformatmonth', $cleanParam)) ? 'MM' : $cleanParam['rangeformatmonth'];
+    $cleanParam['rangeformatday'] = (!array_key_exists('rangeformatday', $cleanParam)) ? 'dd' : $cleanParam['rangeformatday'];
 
     $cleanParam['rangestart'] = (!array_key_exists('rangestart', $cleanParam)) ? date('Y-m-d', strtotime('-1 month')) : $cleanParam['rangestart'];
     $cleanParam['rangeend'] = (!array_key_exists('rangeend', $cleanParam)) ? date('Y-m-d', time('now')) : $cleanParam['rangeend'];
 
-    $cleanParam['locale'] = (!array_key_exists('locale', $cleanParam)) ? 'en-US' : $cleanParam['locale'];
+    $cleanParam['locale'] = (!array_key_exists('locale', $cleanParam)) ? 'en-GB' : $cleanParam['locale'];
 
     $html = '<div id="archive_list">';
     
@@ -51,6 +52,24 @@ function smarty_function_daterange_calendar_html($p_params = array(), &$p_smarty
     $cleanParam['end']['year'] = date('Y', strtotime($cleanParam['rangeend']));
     $cleanParam['end']['month'] = date('m', strtotime($cleanParam['rangeend']));
     $cleanParam['end']['day'] = date('d', strtotime($cleanParam['rangeend']));
+
+    $dateFormatter['day'] = \IntlDateFormatter::create(
+      $cleanParam['locale'],
+      \IntlDateFormatter::NONE,
+      \IntlDateFormatter::NONE,
+      \date_default_timezone_get(),
+      \IntlDateFormatter::GREGORIAN,
+      $cleanParam['rangeformatday']
+    );
+
+    $dateFormatter['month'] = \IntlDateFormatter::create(
+      $cleanParam['locale'],
+      \IntlDateFormatter::NONE,
+      \IntlDateFormatter::NONE,
+      \date_default_timezone_get(),
+      \IntlDateFormatter::GREGORIAN,
+      $cleanParam['rangeformatmonth']
+    );
 
     $html .= '<ol>';
 
@@ -72,7 +91,7 @@ function smarty_function_daterange_calendar_html($p_params = array(), &$p_smarty
             }
 
             $monthString .= '<li><h3><a href="?from='.$year.'-'.$month.'-'.$beginDay.'&to='.$year.'-'.$month.'-'.$maxDay.'">';
-            $monthString .= date($cleanParam['rangeformatmonth'], strtotime($year.'-'.$month));
+            $monthString .= $dateFormatter['month']->format(new \DateTime($year.'-'.$month));
             $monthString .= '</h3>';
 
             $dayString = '<ol class="month m'.$month.'">';
@@ -82,7 +101,7 @@ function smarty_function_daterange_calendar_html($p_params = array(), &$p_smarty
                 $week = 'w'.date('W', strtotime($currIterateDate));
                 $dayoftheweek = 'd'.date('N', strtotime($currIterateDate));
                 $dayString .= ' class="day '.$week.' '.$dayoftheweek.'"';
-                $dayString .= '><a href="?from='.$currIterateDate.'&to='.$currIterateDate.'">'. date($cleanParam['rangeformatday'], strtotime($currIterateDate)).'</a></li>';
+                $dayString .= '><a href="?from='.$currIterateDate.'&to='.$currIterateDate.'">'.$dateFormatter['day']->format(new \DateTime($currIterateDate)).'</a></li>';
             }
             $dayString .= '</ol>';
             $monthString .= $dayString;
